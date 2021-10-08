53bf350474356b33bf89120f14165378c3703017
navodya093 <navodya@iq.lk>
2021-10-08 19:46:46 +0530

    final changes for the live release

---
+++ DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use App\Modules\UserModule\Models\UserProfile;
+use Auth;
+
+class DashboardController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+}
